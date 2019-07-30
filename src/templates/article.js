4c931aaccbed82ed3394355c8c588b86c1a09251
Mohim Ali <mohim2k13@gmail.com>
2019-07-30 22:02:25 +0100

    axios

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,17 +2,17 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
-import Reactmarkdown from "react-markdown"
+import ReactMarkdown from "react-markdown"
 
 const ArticleTemplate = ({data}) => (
     <Layout>
-        <h1>{data.strapiArticle.title}</h1>
+        <h1>XXXXXXX</h1>
         <p>by <Link to={`/authors/User_${data.strapiArticle.authora.id}`}>{data.strapiArticle.authora.username}</Link>
         </p>
         <Img fluid={data.strapiArticle.image.childImageSharp.fluid}/>
-        <Reactmarkdown
-            source={data.strapiArticle.content}
-        />
+            <h1>testttttt</h1>
+            <ReactMarkdown source={data.strapiArticle.content} />
+
     </Layout>
 )
 
